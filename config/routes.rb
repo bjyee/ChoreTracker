6016e683714ce848cc90723d3af97ba19373e1fd
Brandon Yee <bjyee@andrew.cmu.edu>
2011-02-09 20:05:26 -0500

    changed routes and deleted index.html

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ ChoreTracker::Application.routes.draw do
   resources :tasks
 
   resources :children
+  
+  root :to => 'chores#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
